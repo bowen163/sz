63b8ac54aebf71dc397b8d783eac3c0f2945ab5a
bowen163 <763893371@qq.com>
2019-10-18 08:21:48 +0800

    Initial Commit

---
+++ sz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sz
+//
+//  Created by s20181102936 on 2019/10/18.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
